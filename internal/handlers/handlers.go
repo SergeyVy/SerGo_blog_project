b59aed81057d1d41763e6da29cc9124c21cca8b5
Sergey Vydumkin <sergvydum@gmail.com>
2025-08-16 16:20:24 +0530

    fix: export Storage field in Handler

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -3,10 +3,13 @@ package handlers
 import "my-notes-app/storage"
 
 type Handler struct {
-	store  *storage.Storage
-	secret string
+	Storage *storage.Storage
+	secret  string
 }
 
 func NewHandler(store *storage.Storage, secret string) *Handler {
-	return &Handler{store: store, secret: secret}
+	return &Handler{
+		Storage: store,
+		secret:  secret,
+	}
 }
